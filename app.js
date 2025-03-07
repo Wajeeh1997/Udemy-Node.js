b83f0354844d86e8d66cd001bf459587a6e9efcb
Wajeeh Rizvi <rizviwajih@gmail.com>
2025-03-08 03:09:10 +0500

    Node.js serv

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-var http = require('http');
+// Importing the HTTP module
+const http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+// Define the host and port
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create a server instance
+const server = http.createServer((req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  // Send the response body "Hello World"
+  res.end('This world is exactly like me and i am like Jawwad Shamsi\n');
+});
+
+// Start the server and listen for incoming connections
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
