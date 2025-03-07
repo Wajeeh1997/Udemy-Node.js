e02e7b2bcd128ee46bd5986d0cac57a5ef756228
Wajeeh Rizvi <rizviwajih@gmail.com>
2025-03-08 03:19:03 +0500

    experimentation

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-// Importing the HTTP module
-const http = require('http');
+// // Importing the HTTP module
+// const http = require('http');
 
-// Define the host and port
-const hostname = '127.0.0.1';
-const port = 3000;
+// // Define the host and port
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-// Create a server instance
-const server = http.createServer((req, res) => {
-  // Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  // Send the response body "Hello World"
-  res.end('Programming is shit\n');
-});
+// // Create a server instance
+// const server = http.createServer((req, res) => {
+//   // Set the response HTTP header with HTTP status and Content type
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   // Send the response body "Hello World"
+//   res.end('Programming is shit\n');
+// });
 
-// Start the server and listen for incoming connections
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// // Start the server and listen for incoming connections
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
